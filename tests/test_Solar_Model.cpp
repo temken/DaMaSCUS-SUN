0974c63153aa151b0a9b06b21878a517f345f3f2
Timon Emken <emken@chalmers.se>
2020-06-02 17:19:53 +0200

    Add unit tests for SolarModel

--- tests/test_Solar_Model.cpp
+++ tests/test_Solar_Model.cpp
@@ -1,3 +1,69 @@
 #include "gtest/gtest.h"
 
+#include <cmath>
+
+// Headers from libphysica
+#include "Natural_Units.hpp"
+
 #include "Solar_Model.hpp"
+
+using namespace libphysica::natural_units;
+
+TEST(TestSolarModel, TestSolarNucleus)
+{
+	// ARRANGE
+	double rho	= 1.505e+02 * gram / cm / cm / cm;
+	double f_H1 = 0.36209;
+	double n_H1 = f_H1 * rho / mProton;
+	Solar_Model SSM;
+	// ACT & ASSERT
+	ASSERT_DOUBLE_EQ(SSM.nuclear_targets[0].Number_Density(0.0), n_H1);
+	ASSERT_DOUBLE_EQ(SSM.nuclear_targets[0].Number_Density(0.001 * rSun), n_H1);
+	ASSERT_DOUBLE_EQ(SSM.nuclear_targets[0].Number_Density(0.00150 * rSun), n_H1);
+}
+
+TEST(TestSolarModel, TestConstructor)
+{
+	// ARRANGE
+	// ACT
+	Solar_Model SSM;
+	// ASSERT
+	ASSERT_EQ(SSM.name, "Standard Solar Model AGSS09");
+	ASSERT_NE(SSM.nuclear_targets.size(), 0);
+}
+
+TEST(TestSolarModel, TestMass)
+{
+	// ARRANGE
+	Solar_Model SSM;
+	// ACT & ASSERT
+	ASSERT_DOUBLE_EQ(SSM.Mass(0), 0.0);
+	ASSERT_DOUBLE_EQ(SSM.Mass(rSun), mSun);
+	ASSERT_DOUBLE_EQ(SSM.Mass(0.00150 * rSun), 0.0000004 * mSun);
+}
+
+TEST(TestSolarModel, TestTemperature)
+{
+	// ARRANGE
+	Solar_Model SSM;
+	// ACT & ASSERT
+	ASSERT_DOUBLE_EQ(SSM.Temperature(0), 1.549e+07 * Kelvin);
+	ASSERT_DOUBLE_EQ(SSM.Temperature(rSun), 7.063e+04 * Kelvin);
+	ASSERT_DOUBLE_EQ(SSM.Temperature(0.96950 * rSun), 1.544e+05 * Kelvin);
+}
+
+TEST(TestSolarModel, TestLocalEscapeSpeed)
+{
+	// ARRANGE
+	Solar_Model SSM;
+	// ACT & ASSERT
+	ASSERT_NEAR(SSM.Local_Escape_Speed(0), 1384.6 * km/sec, 0.1 * km/sec);
+	ASSERT_DOUBLE_EQ(SSM.Local_Escape_Speed(rSun), sqrt(2.0 * G_Newton * mSun / rSun));
+	ASSERT_DOUBLE_EQ(SSM.Local_Escape_Speed(2.0 * rSun), sqrt(2.0 * G_Newton * mSun / 2.0 / rSun));
+}
+
+// TEST(TestSolarModel, TestPrintSummary)
+// {
+// 	// ARRANGE
+// 	// ACT & ASSERT
+// }
